343e663783901c996b0a2594c0e4f507b3fd98d3
Kudrat Aggarwal <kaggarwal5_be24@thapar.edu>
2025-09-14 13:48:14 +0530

    KUDRAT 1024240069

---
+++ 1(b).py
@@ -0,0 +1,9 @@
+    def insert_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
