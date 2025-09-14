2a6d3c5a7b61a618d15d809fec3afae7e0f17cd4
Kudrat Aggarwal <kaggarwal5_be24@thapar.edu>
2025-09-14 13:47:12 +0530

    KUDRAT 1024240069

---
+++ 1(a).py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_begin(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("NULL")
+
+
+# Example
+sll = SinglyLinkedList()
+sll.insert_begin(10)
+sll.insert_begin(20)
+sll.display()
